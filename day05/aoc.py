e3822fa2aa28548e8c37621c1e12a951fac77236
Logan Farci <farci.logan@gmail.com>
2020-12-08 21:52:20 +0100

    day05: the part 2 is complete

--- day05/aoc.py
+++ day05/aoc.py
@@ -34,10 +34,20 @@ def find_seat_id(boarding_pass, row_selector_len = 7):
     col_selector = boarding_pass[row_selector_len:]
     return seat_id(find_row(row_selector), find_column(col_selector))
 
+def find_my_seat(seats_ids):
+    sorted_seat_ids = sorted(seats_ids)
+    last_seat_id = seats_ids[0]
+    for i in range(1, len(sorted_seat_ids)):
+        if (sorted_seat_ids[i] - last_seat_id) > 1:
+            return sorted_seat_ids[i] - 1
+        last_seat_id = sorted_seat_ids[i]
+    return None
+
 def main():
     boarding_passes = read_boarding_passes_from_input("./input.txt")
     seat_ids = [find_seat_id(bp) for bp in boarding_passes]
-    print(f"The highest seat ID on a boarding pass is {max(seat_ids)}")
+    print(f"The highest seat ID on a boarding pass is {max(seat_ids)} (part 1)")
+    print(f"My seat id is {find_my_seat(seat_ids)} (part 2)")
 
 if __name__ == "__main__":
     main()
