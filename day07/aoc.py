f5f359a63db1d7e576d8790f73d87d8b687bbe7d
Logan Farci <farci.logan@gmail.com>
2020-12-11 22:43:45 +0100

    day07: ⭐⭐

--- day07/aoc.py
+++ day07/aoc.py
@@ -7,7 +7,7 @@ def read_children_strings(line):
 
 def read_child(child_string):
     color_strings = child_string.split()[1:3]
-    return " ".join(color_strings)
+    return { "amount": int(child_string[0]), "color": " ".join(color_strings) }
 
 def read_children(children_string, empty_bag_string = "no other bags"):
     children_strings = read_children_strings(children_string)
@@ -36,12 +36,12 @@ def exist_way_to(root_color, target_color, rules):
         return False
     else:
         flags = []
-        for color in rules[root_color]:
-            if color == target_color:
+        for child in rules[root_color]:
+            if child["color"] == target_color:
                 return True
             else:
-                flags.append(exist_way_to(color, target_color, rules))
-    return any(flags)
+                flags.append(exist_way_to(child["color"], target_color, rules))
+        return any(flags)
 
 def count_possible_bad_parent_for(child_color, rules):
     count = 0
@@ -50,10 +50,22 @@ def count_possible_bad_parent_for(child_color, rules):
             count += 1
     return count
 
+def count_children(root_color, nchildren, rules):
+    if len(rules[root_color]) == 0:
+        return nchildren
+    else:
+        flags = []
+        for child in rules[root_color]:
+            ngrandchildren = count_children(child["color"], 0, rules)
+            nchildren += child["amount"] + (child["amount"] * ngrandchildren)
+        return nchildren
+
 def main():
     rules = read_rules_from_input("./input.txt")
-    count = count_possible_bad_parent_for('shiny gold', rules)
-    print(f"Number of bag colors that can contain shiny gold bags: {count} (part 1)")
+    count_part_01 = count_possible_bad_parent_for('shiny gold', rules)
+    count_part_02 = count_children('shiny gold', 0, rules)
+    print(f"Number of bag colors that can contain shiny gold bags: {count_part_01} (part 1)")
+    print(f"Number of required individual bags: {count_part_02} (part 2)")
 
 if __name__ == "__main__":
     main()
